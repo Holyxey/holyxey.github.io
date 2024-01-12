cc486dd9d98a40fe40edc56e59843fc10bdfa90f
Holyxey <text.holyxey@gmail.com>
2024-01-12 23:46:32 +0400

    Update eltMobLang.js

--- public/eltMobLang.js
+++ public/eltMobLang.js
@@ -1,21 +1,8 @@
 let mobMenuBlock = document.getElementById('mobLang');
 let mobLinks = document.getElementsByClassName('mobLangLink');
-let menuButtonHolyxey = document.querySelectorAll('data-w-id'["95b06c38-efdf-9b0b-0d7c-dfd5ca0d96b3"]);
-
 
 for (let i = 0; i < mobLinks.length; i++) {
-    mobLinks[i].style.display = 'none';
-    mobLinks[i].style.opacity = '0';
+    setTimeout(function (){
+        mobLinks[i].classList.add("linkShow");
+    }, (100 + (i * 1000)));
 }
-
-function eltMobLang() {
-    let i = 0;
-            setInterval(function (){
-                // mobLinks[i].style.opacity = '0';
-                mobLinks[i].style.display = 'block';
-                mobLinks[i].classList.add("linkShow");
-                i++;
-            }, 400);
-}
-//
-eltMobLang();
