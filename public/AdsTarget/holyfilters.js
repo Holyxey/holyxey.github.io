df681154a92bdd2909493ab733e8453d8a3904db
Holyxey <text.holyxey@gmail.com>
2024-01-14 05:56:34 +0400

    Update holyfilters.js

--- public/AdsTarget/holyfilters.js
+++ public/AdsTarget/holyfilters.js
@@ -27,10 +27,14 @@ function defaultVisible() {
             if (findingCategories[i].classList.contains('filterActive')) {
                 console.log('Показываем все элементы в категории ' + findingCategories[i].innerHTML);
                 categoryElements[h].style.cssText += ('animation: showElements .5s ease-out forwards');
-                categoryElements[h].removeAttribute('hidden');
+                let elementDisplay = categoryElements[h].style.display;
+                categoryElements[h].style.display = elementDisplay;
+
+                // categoryElements[h].removeAttribute('hidden');
             } else {
                 console.log('Класс не найден, скрываем все элементы в категории ' + findingCategories[i].innerHTML);
-                categoryElements[h].setAttribute('hidden', '');
+                // categoryElements[h].setAttribute('hidden', '');
+                categoryElements[h].style.display = 'none';
             }
         }
 
