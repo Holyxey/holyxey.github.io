e6ed9bf5f814dab65181766a87b5580939deec64
Holyxey <text.holyxey@gmail.com>
2024-01-14 04:01:48 +0400

    Update eltMobLang.js

--- public/elTeacher/eltMobLang.js
+++ public/elTeacher/eltMobLang.js
@@ -6,3 +6,12 @@ for (let i = 0; i < mobLinks.length; i++) {
         mobLinks[i].classList.add("linkShow");
     }, (100 + (i * 1000)));
 }
+
+// Добавим отступов в мобильной версии
+let footerCheck = document.getElementsByClassName('mainFooterBlock');
+function footerPadding() {
+    footerCheck[0].style.paddingBottom = '80px';
+}
+if (document.documentElement.clientWidth < 600) {
+    footerPadding()
+} else {};
