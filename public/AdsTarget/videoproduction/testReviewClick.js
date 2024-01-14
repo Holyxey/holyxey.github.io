a7c702a4692c73eeb9ccc52f8541cd32d42d7e12
Holyxey <text.holyxey@gmail.com>
2024-01-14 08:00:02 +0400

    Create testReviewClick.js

---
+++ public/AdsTarget/videoproduction/testReviewClick.js
@@ -0,0 +1,17 @@
+
+let setI = 0;
+
+function testReviewClick(el) {
+    
+    if (setI == 0) {
+        el.style.cssText = "height:unset; max-height:unset";
+        setI++;
+    } else {
+        el.style.cssText = "height:40vh; max-height:350px";
+        setI--;
+    } 
+}
+function IisUno(el) {
+    el.style.cssText = "height:40vh; max-height:350px";
+    setI = 0;
+}
