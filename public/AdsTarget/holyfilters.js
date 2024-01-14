2cc80bf0f717f675f0694c9c78e9583f8bde33e8
Holyxey <text.holyxey@gmail.com>
2024-01-14 10:01:07 +0400

    Update holyfilters.js

--- public/AdsTarget/holyfilters.js
+++ public/AdsTarget/holyfilters.js
@@ -34,7 +34,7 @@ function defaultVisible() {
                     categoryElements[h].style.display = 'none';
                     categoryElements[h].classList.remove('showThisElement');
                 }
-            }, (30 * h));
+            }, (50 * h));
         }
 
     }
