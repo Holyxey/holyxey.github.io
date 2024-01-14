bb96dd7cbaf534e5c45b2d1edf0f0869ce056939
Holyxey <text.holyxey@gmail.com>
2024-01-14 05:04:50 +0400

    Update holyfilters.js

--- public/AdsTarget/holyfilters.js
+++ public/AdsTarget/holyfilters.js
@@ -21,14 +21,14 @@ function defaultVisible() {
 
         for (let h = 0; h < categoryElements.length; h++) {
             if (findingCategories[i].classList.contains('filterActive')) {
-                categoryElements[h].style.display = 'unset';
+                categoryElements[h].style.cssText -= 'display: none';
                 console.log('Показываем все элементы в категории ' + findingCategories[i].innerHTML);
                 categoryElements[h].style.cssText += ('animation: showElements .5s ease-out forwards');
                 categoryElements[h].removeAttribute('hidden');
             } else {
             //    console.log('Класс не найден, скрываем все элементы в категории ' + findingCategories[i].innerHTML);
                 categoryElements[h].setAttribute('hidden', '');
-                categoryElements[h].style.display = 'none';
+                categoryElements[h].style.cssText += 'display: none';
             }
         }
 
