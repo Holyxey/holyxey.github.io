8c8d1d9d57e28884036f2fc883ebf54c56d68cce
Holyxey <152817274+Holyxey@users.noreply.github.com>
2024-01-16 04:04:10 +0400

    Update case_links.js

--- public/AdsTarget/videoproduction/case_links.js
+++ public/AdsTarget/videoproduction/case_links.js
@@ -1,9 +1,9 @@
 
 function caseLink(el) {
-    if (document.documentElement.clientWidth > 500) {
+    // if (document.documentElement.clientWidth > 500) {
     let linkElement = el.querySelector('a');
 
     if (linkElement && linkElement.href) 
         window.open(linkElement.href, '_blank');
-    }
+    // }
 }
