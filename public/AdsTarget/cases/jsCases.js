b14d1ac527eb54b25629511d6fa2bd6abbfd3e81
Holyxey <text.holyxey@gmail.com>
2024-01-16 22:06:53 +0400

    rename_attributes

--- public/AdsTarget/cases/jsCases.js
+++ public/AdsTarget/cases/jsCases.js
@@ -1,11 +1,11 @@
-let allCases = document.getElementsByClassName('caseBlock');
+let allCases = document.getElementsByClassName('h_at_caseBlock');
 
 for (let i = 0; i < allCases.length; i++) {
     allCases[i].setAttribute('onclick', 'caseLink(this)');
 
-    let caseBlockHeader = allCases[i].querySelectorAll('.caseBlockHeader');
-    let caseBlockImg = allCases[i].querySelectorAll('.caseBlockImg')
-    let caseBlockLink = allCases[i].querySelectorAll('.caseBlockLink')
+    let caseBlockHeader = allCases[i].querySelectorAll('.h_at_caseBlockHeader');
+    let caseBlockImg = allCases[i].querySelectorAll('.h_at_caseBlockImg')
+    let caseBlockLink = allCases[i].querySelectorAll('.h_at_caseBlockLink')
 
     for (let r = 0; r < caseBlockLink.length; r++)
         caseBlockLink[r].setAttribute('target', '_blank')
