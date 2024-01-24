139d14a12586d68b41f9632cdb7bc73e9b65f600
Holyxey <text.holyxey@gmail.com>
2024-01-24 08:10:30 +0400

    devPlus

---
+++ public/AdsTarget/Development/dev.js
@@ -0,0 +1,27 @@
+let step = document.getElementsByClassName('step');
+let stepS = document.getElementById('dev-steps');
+
+//
+for (let i = 0; i < step.length; i++) {
+    step[i].setAttribute('step-n', (1 + i));
+    step[i].addEventListener('mouseenter', function() { stepHover(this); });
+    step[i].addEventListener('mouseleave', function() { stepLeave(this); });
+    // step[i].setAttribute('onmouseenter', 'stepHover(this)');
+    // step[i].setAttribute('onmouseleave', 'stepLeave(this)');
+}
+function stepHover(el) {
+    el.classList.add('step-hover');
+    let n = el.getAttribute('step-n');
+    if (n < step.length) {
+        let nxt = stepS.querySelector('[step-n="' + (parseInt(n) + 1) + '"]');
+        nxt.classList.add('step-hover-sec');
+    }
+}
+function stepLeave(el) {
+    el.classList.remove('step-hover')
+    let n = el.getAttribute('step-n');
+    if (n < step.length) {
+        let nxt = stepS.querySelector('[step-n="' + (parseInt(n) + 1) + '"]');
+        nxt.classList.remove('step-hover-sec');
+    }
+}
