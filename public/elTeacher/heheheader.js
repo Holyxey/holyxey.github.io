3196e582e6898433c6d5f8fb7ae3bdff6572d01f
Holyxey <text.holyxey@gmail.com>
2024-01-25 14:17:51 +0400

    Create heheheader.js

---
+++ public/elTeacher/heheheader.js
@@ -0,0 +1,5 @@
+function sethehe() {
+    let heheheader = document.getElementsByClassName('heheheader')
+    heheheader[0].style.cssText += "position: fixed; top: 0; left: 0; z-index: 100500";
+}
+document.addEventListener("DOMContentLoaded", sethehe)
