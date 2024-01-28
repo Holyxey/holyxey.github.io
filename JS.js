9b289cd7efe1c82c6fa79325e7da21e6226808e1
Holyxey <text.holyxey@gmail.com>
2024-01-28 04:06:37 +0400

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,11 +1,5 @@
-let holyxeyCom = document.createComment('Designed by Holyxey at AdsTarget');
-document.head.prepend(holyxeyCom);
-
-// function holyLink() {
-//     let holyLink = document.createElement('a');
-//     holyLink.href = 'https://holyxey.com';
-//     holyLink.style.content = 'url("https://holyxey.github.io/holyxey/hlx.png")';
-//     holyLink.style.cssText += 'position: absolute; bottom: 0; left: 0;';
-//     document.body.appendChild(holyLink);
-// }
-// holyLink();
+function holyxey() {
+    let addCommentHolyxey = document.createComment('Holyxey at AdsTarget is working on the site');
+    document.documentElement.prepend(addCommentHolyxey);
+}
+document.addEventListener("DOMContentLoaded", holyxey)
