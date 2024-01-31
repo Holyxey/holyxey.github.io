c4a1231f21a5cd4dbb1b99d30525e4cd9883a18b
Holyxey <text.holyxey@gmail.com>
2024-01-31 19:07:40 +0400

    hlds

--- public/Welton/Holydays/holidays.js
+++ public/Welton/Holydays/holidays.js
@@ -2,6 +2,7 @@ let hldsBlock;
 let hldsItem;
 let hldsClose;
 let hldsPopup;
+let hldsOpen;
 
 window.addEventListener("DOMContentLoaded", hldsStart);
 
@@ -10,8 +11,11 @@ function hldsStart(el) {
     hldsItem = document.querySelectorAll('.hx-wh-popup-item');
     hldsClose = document.getElementById('hx-wh-hlds-cls');
     hldsPopup = document.getElementById('hx-wh-popup');
+    hldsOpen = document.querySelector('a', '#hlds');
+
     hldsClose.setAttribute('onclick', 'hldsCloseFun()')
     hldsPopup.setAttribute('onclick', 'hldsCloseFun()')
+    hldsOpen.setAttribute('onclick', 'hldsOpenPopup()')
 }
 
 function hldsCloseFun() {
@@ -19,5 +23,13 @@ function hldsCloseFun() {
     setTimeout(function () {
         hldsPopup.classList.remove('hx-wh-hlds-hide')
         hldsPopup.style.display = 'none'
+    }, 400)
+}
+
+function hldsOpenPopup() {
+    hldsPopup.style.display = 'flex'
+    hldsPopup.classList.add('hx-wh-hlds-show')
+    setTimeout(function () {
+        hldsPopup.classList.remove('hx-wh-hlds-show')
     }, 400)
 }
