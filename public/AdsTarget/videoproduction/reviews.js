312211fd8d43baf38a024d675ad92f1c46fedeb4
Holyxey <text.holyxey@gmail.com>
2024-02-01 19:53:41 +0400

    vid

--- public/AdsTarget/videoproduction/reviews.js
+++ public/AdsTarget/videoproduction/reviews.js
@@ -4,3 +4,19 @@ $('.hx-at-review-block').slick({
     slidesToScroll: 1,
     autoplay: true
 });
+
+// Картинки последовательно грузим
+document.addEventListener("DOMContentLoaded", reviewImgWait)
+let caseBackgr;
+function reviewImgWait() {
+    caseBackgr = document.getElementsByClassName('hx-at-case-web');
+    for (let i = 0; i < caseBackgr.length; i++) {
+        let elImg = caseBackgr[i].querySelector('div[laazy]')
+        if (elImg) {
+            let url = elImg.getAttribute('laazy')
+            console.log(url)
+            elImg.style.backgroundImage = url;
+            elImg.classList.add('showpopup')
+        }
+    }
+}
