1c25b3aae29b7c2c00670f25c5ebeedcdb19f7f1
Holyxey <text.holyxey@gmail.com>
2024-02-03 05:11:29 +0400

    vdprdctn

--- public/AdsTarget/videoproduction/vid.js
+++ public/AdsTarget/videoproduction/vid.js
@@ -4,5 +4,6 @@ function hxAtVidHero(el) {
     let vidsctn = document.getElementById('hx-at-hero-video');
     vidsctn.style.opacity = '0';
     vidsctn.classList.add('slowshow');
-    vidsctn.src = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4';
+    vidsctn.src = 'https://vk.com/video_ext.php?oid=-20458507&id=456239141&hd=4&autoplay=1';
+    // vidsctn.src = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4';
 }
