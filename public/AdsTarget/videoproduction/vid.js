c871f94239e44741766aca9030c072c116375b32
Holyxey <text.holyxey@gmail.com>
2024-02-03 18:31:40 +0400

    mov

--- public/AdsTarget/videoproduction/vid.js
+++ public/AdsTarget/videoproduction/vid.js
@@ -4,7 +4,7 @@ function hxAtVidHero(el) {
     let vidsctn = document.getElementById('hx-at-hero-video');
     // vidsctn.style.opacity = '0';
     // vidsctn.classList.add('slowshow');
-    vidsctn.src = 'https://holyxey.github.io/public/AdsTarget/videoproduction/8b1f7c66.WebM';
+    vidsctn.src = 'https://holyxey.github.io/public/AdsTarget/videoproduction/8b1f7c66.mov';
     // vidsctn.src = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4';
 }
 
