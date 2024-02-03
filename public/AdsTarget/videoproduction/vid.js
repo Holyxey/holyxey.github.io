4f880aee1ffc1c1858b3d364dfd72e1e6930f7b0
Holyxey <text.holyxey@gmail.com>
2024-02-03 17:49:42 +0400

    nvid

--- public/AdsTarget/videoproduction/vid.js
+++ public/AdsTarget/videoproduction/vid.js
@@ -4,6 +4,6 @@ function hxAtVidHero(el) {
     let vidsctn = document.getElementById('hx-at-hero-video');
     vidsctn.style.opacity = '0';
     vidsctn.classList.add('slowshow');
-    vidsctn.src = 'https://vk.com/video_ext.php?oid=-20458507&id=456239141&hd=4&autoplay=1';
+    vidsctn.src = 'https://167vod-adaptive.akamaized.net/exp=1706971669~acl=%2Fa06c2cb7-d91c-4a25-9fbc-5c976a86dfde%2F%2A~hmac=1c786376a4416bc253b11f4f1f5e2854c42b3cb26f115ca265674256633f336b/a06c2cb7-d91c-4a25-9fbc-5c976a86dfde/parcel/video/7b077004.mp4';
     // vidsctn.src = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4';
 }
