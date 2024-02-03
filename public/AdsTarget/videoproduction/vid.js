4692d7d3dae8781ae30e65199a5e71d0fc6fe11f
Holyxey <text.holyxey@gmail.com>
2024-02-03 18:44:18 +0400

    Update vid.js

--- public/AdsTarget/videoproduction/vid.js
+++ public/AdsTarget/videoproduction/vid.js
@@ -2,9 +2,7 @@ window.addEventListener("DOMContentLoaded", hxAtVidHero)
 
 function hxAtVidHero(el) {
     let vidsctn = document.getElementById('hx-at-hero-video');
-    // vidsctn.style.opacity = '0';
-    // vidsctn.classList.add('slowshow');
     vidsctn.src = 'https://holyxey.github.io/public/AdsTarget/videoproduction/8b1f7c66.mov';
-    // vidsctn.src = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4';
+    vidsctn.load()
 }
 
