3f80ff86123a82fd9e5afa8289b851d416da9aae
Holyxey <text.holyxey@gmail.com>
2024-02-03 18:10:30 +0400

    nvid

--- public/AdsTarget/videoproduction/vid.js
+++ public/AdsTarget/videoproduction/vid.js
@@ -4,7 +4,7 @@ function hxAtVidHero(el) {
     let vidsctn = document.getElementById('hx-at-hero-video');
     vidsctn.style.opacity = '0';
     // vidsctn.classList.add('slowshow');
-    vidsctn.src = 'https://167vod-adaptive.akamaized.net/exp=1706972578~acl=%2Fa06c2cb7-d91c-4a25-9fbc-5c976a86dfde%2F%2A~hmac=d4e43b23cc5749ef78619dcadda455992e87d8e883539ad54abba9fcc9ed0391/a06c2cb7-d91c-4a25-9fbc-5c976a86dfde/parcel/video/7b077004.mp4';
+    vidsctn.src = 'https://v3.cdnpk.net/videvo_files/video/free/2015-08/large_preview/Ao_Nang_Beach_Yoga_MP4_HDV_1080p25__TanuriX_Stock_Footage_NS.mp4';
     // vidsctn.src = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4';
 }
 
