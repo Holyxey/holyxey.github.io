66e8c56031335672ada524f45e2778a9248df317
Holyxey <text.holyxey@gmail.com>
2024-02-03 18:14:34 +0400

    Update vid.js

--- public/AdsTarget/videoproduction/vid.js
+++ public/AdsTarget/videoproduction/vid.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", hxAtVidHero)
 
 function hxAtVidHero(el) {
     let vidsctn = document.getElementById('hx-at-hero-video');
-    vidsctn.style.opacity = '0';
+    // vidsctn.style.opacity = '0';
     // vidsctn.classList.add('slowshow');
     vidsctn.src = 'https://v3.cdnpk.net/videvo_files/video/free/2015-08/large_preview/Ao_Nang_Beach_Yoga_MP4_HDV_1080p25__TanuriX_Stock_Footage_NS.mp4';
     // vidsctn.src = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4';
