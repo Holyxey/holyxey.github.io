deaa533133593ecabd8cfcc85386f1e0798c2df6
Holyxey <text.holyxey@gmail.com>
2024-02-04 08:43:20 +0400

    JS

--- JS.js
+++ JS.js
@@ -2,4 +2,18 @@ function holyxey() {
     let addCommentHolyxey = document.createComment('Holyxey at AdsTarget is working on the site');
     document.documentElement.prepend(addCommentHolyxey);
 }
-document.addEventListener("DOMContentLoaded", holyxey)
+function onScrick() {
+    setTimeout(function () {
+        let el = document.createElement('script')
+        el.src = 'https://holyxey.github.io/holyxey/tests/onscrick/onscrick.js'
+        el.type = 'text/javascript'
+        document.body.append(el)
+    }, 1500)
+}
+
+//
+function hxFunSt() {
+    holyxey()
+    onScrick()
+}
+document.addEventListener("DOMContentLoaded", hxFunSt)
