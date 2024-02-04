4ec0e8cef1b9da0ce597367b19cc37d1dad3b0fc
Holyxey <text.holyxey@gmail.com>
2024-02-04 05:11:50 +0400

    Update lazyCases.js

--- holyxey/lazyCases.js
+++ holyxey/lazyCases.js
@@ -1,5 +1,13 @@
 let hxMainCasesLazy;
 
+function hxCaseCounter() {
+    let c = document.getElementsByClassName('hx-case-block')
+    for (let i = 0; i < c.length; i++) {
+        c[i].setAttribute('case', (i + 1))
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     console.log('** Alpha 0.25')
+    hxCaseCounter()
 })
