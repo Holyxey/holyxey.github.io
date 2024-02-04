aa06f580bdbf65e62e45893c77d5216d5e0d19e4
Holyxey <text.holyxey@gmail.com>
2024-02-04 09:27:52 +0400

    Update lang.js

--- holyxey/lang.js
+++ holyxey/lang.js
@@ -1,4 +1,7 @@
-if (navigator.language.includes('ru'))
-    location = '/ru'
-if (navigator.language.includes('en'))
-    location = '/'
+function hxChechLang() {
+    if (navigator.language.includes('ru'))
+        location = '/ru'
+    if (navigator.language.includes('en'))
+        location = '/'
+}
+hxChechLang()
