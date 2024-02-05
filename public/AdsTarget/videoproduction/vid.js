905b43ed8a3861f26a4e800d6f48e191b4f304fc
Holyxey <text.holyxey@gmail.com>
2024-02-05 14:41:26 +0400

    newvid

--- public/AdsTarget/videoproduction/vid.js
+++ public/AdsTarget/videoproduction/vid.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", hxAtVidHero)
 
 function hxAtVidHero(el) {
     let vidsctn = document.getElementById('hx-at-hero-video');
-    vidsctn.src = 'https://holyxey.github.io/public/AdsTarget/videoproduction/8b1f7c66.mov';
+    vidsctn.src = 'https://holyxey.com/public/AdsTarget/videoproduction/hero.mov';
     vidsctn.load()
 }
 
