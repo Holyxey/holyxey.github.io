aff54ca10102fc02df61ef3c1f0c0728675fd00d
Holyxey <text.holyxey@gmail.com>
2024-02-09 02:32:26 +0400

    Update JS.js

--- JS.js
+++ JS.js
@@ -2,16 +2,8 @@ function holyxey() {
     let addCommentHolyxey = document.createComment('Site design and supporting at AdsTarget by Holyxey');
     document.documentElement.prepend(addCommentHolyxey);
 }
-function onScrick() {
-        let el = document.createElement('script')
-        el.src = 'https://holyxey.github.io/holyxey/tests/onscrick/onscrick.js'
-        el.type = 'text/javascript'
-        document.body.append(el)
-}
-
 //
 function hxFunSt() {
     holyxey()
-    onScrick()
 }
 document.addEventListener("DOMContentLoaded", hxFunSt)
