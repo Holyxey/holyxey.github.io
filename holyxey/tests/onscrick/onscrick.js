a827e9b3e64387e2a4d546fa249ca309d046e5b8
Holyxey <text.holyxey@gmail.com>
2024-02-09 02:25:19 +0400

    Update onscrick.js

--- holyxey/tests/onscrick/onscrick.js
+++ holyxey/tests/onscrick/onscrick.js
@@ -25,6 +25,7 @@ function onScrick() {
         el.target = '_blank'
         el.style.textDecoration = 'none'
         el.style.color = bgcolor
+        el.style.bottom = '0'
         el.style.position = 'absolute'
         document.body.append(el)
     } else {
