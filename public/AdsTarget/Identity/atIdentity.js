5ffc9c0da33a623c13d74b04c70fd2343b7a38d0
Holyxey <text.holyxey@gmail.com>
2024-02-12 13:05:37 +0400

    atReferrerUtm

--- public/AdsTarget/Identity/atIdentity.js
+++
@@ -1,2 +0,0 @@
-//
-let atHrefsAncor = document.querySelectorAll('.hx-at-sct')
--- public/AdsTarget/atReferrerBack.js
+++ public/AdsTarget/atReferrerBack.js
@@ -1,26 +1,10 @@
-// В разработке
-
-function atReferrerBack() {
-    if (document.referrer.includes('adstarget')) {
-        let referrerCreate = document.createElement('a')
-        referrerCreate.href = 'https://adstarget.ru/web-development';
-        referrerCreate.text = 'Назад';
-        referrerCreate.style.cssText = `
-            position: fixed;
-            top: 1rem;
-            left: 1rem;
-            z-index: 995595607309;
-            color: #222222!important;
-            padding: .5rem 1rem;
-            border: 1px solid #5ED7CF;
-            background-color: #ffffff;
-            border-radius: .3rem;
-            text-decoration: none;
-            font-weight: 100;
-            font-family: sans-serif;
-            `;
-        document.body.append(referrerCreate)
+function atAddUtmCases(el) {
+    if (!el.href.includes('adstarget')) {
+        const hasParams = el.href.includes('?');
+        el.href += (hasParams ? '&' : '?') + 'utm_source=AdsTarget';
+        el.setAttribute('onclick', 'atUtmCookie()')
     }
 }
-
-window.addEventListener("load", setTimeout(atReferrerBack, 1500))
+// document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a').forEach(el => atAddUtmCases(el));
+// })
