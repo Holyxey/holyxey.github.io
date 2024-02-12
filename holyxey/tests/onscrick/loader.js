1758e0fb1aad4e5414690db0602847e71773950a
Holyxey <text.holyxey@gmail.com>
2024-02-12 13:17:36 +0400

    delonscrick

--- holyxey/tests/onscrick/loader.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // setTimeout(function () {
-        let el = document.createElement('script')
-        el.src = 'https://holyxey.github.io/holyxey/tests/onscrick/onscrick.js'
-        el.type = 'text/javascript'
-        document.body.prepend(el)
-    // }, 1500)
-})
