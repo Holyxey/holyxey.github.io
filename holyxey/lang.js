dd1fb3ac4f24b5b05ba8f5383892012c623a2fdc
Holyxey <text.holyxey@gmail.com>
2024-02-15 19:10:35 +0400

    portfolio

--- holyxey/lang.js
+++
@@ -1,4 +0,0 @@
-    if (navigator.language.includes('ru') && !navigator.language.includes('en'))
-        location = '/ru'
-    if (navigator.language.includes('en') && !navigator.language.includes('ru'))
-        location = '/'
