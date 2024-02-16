79cea30ccb7549b556c7fde7188bc4ae9bbe1dfc
Holyxey <text.holyxey@gmail.com>
2024-02-16 13:16:14 +0400

    Update atReferrerBack.js

--- public/AdsTarget/atReferrerBack.js
+++ public/AdsTarget/atReferrerBack.js
@@ -1,11 +1,7 @@
 function atAddUtmCases(el) {
     if (!el.href.includes('adstarget')) {
-        let utm = window.location.href
         const hasParams = el.href.includes('?');
-        el.href += (hasParams ? '&' : '?') + 'utm_source=' + utm;
-        el.setAttribute('onclick', 'atUtmCookie()')
+        el.href += (hasParams ? '&' : '?') + 'utm_source=AdsTarget';
     }
 }
-// document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll('a').forEach(el => atAddUtmCases(el));
-// })
