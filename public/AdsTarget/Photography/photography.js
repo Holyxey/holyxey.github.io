a4b0b19ee191c7e2accc3af9aa910992158bb0e3
Holyxey <text.holyxey@gmail.com>
2024-02-21 09:13:00 +0400

    At Photo Popup

--- public/AdsTarget/Photography/photography.js
+++ public/AdsTarget/Photography/photography.js
@@ -1,6 +1,26 @@
 // CASES PARAMETERS ==========
-let phCaseItem = document.getElementsByClassName('hx-at-photo-case-item')
-for (let i = 0; i < phCaseItem.length; i++) {
-    phCaseItem[i].classList.add('case_' + (i + 1))
-    phCaseItem[i].style.cssText += ('background-image: url("imgs/' + (i + 1) + '.jpg")')
-}
+    let phCaseItem = document.getElementsByClassName('hx-at-photo-case-item')
+    for (let i = 0; i < phCaseItem.length; i++) {
+        phCaseItem[i].classList.add('case_' + (i + 1))
+        phCaseItem[i].style.cssText += ('background-image: url("imgs/' + (i + 1) + '.jpg")')
+        phCaseItem[i].setAttribute('onclick', 'phCaseClick(this)')
+    }
+
+// CASES POPUP ==========
+    function phCaseClick(el) {
+        let image = el.style.backgroundImage;
+        let imageUrl = image.slice(5, -2);
+        let createPopupImg = document.createElement('img')
+        let createPopupDiv = document.createElement('div')
+
+        createPopupDiv.classList.add('hx-at-photo-popup-div')
+        createPopupDiv.setAttribute('onclick', 'phClosePopup(this)')
+
+        createPopupImg.classList.add('hx-at-photo-popup')
+        createPopupImg.src = imageUrl;
+        document.body.append(createPopupDiv)
+        createPopupDiv.append(createPopupImg)
+    }
+    function phClosePopup(el) {
+        document.addEventListener("click", el.remove())
+    }
