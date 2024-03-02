ac0e636683066d492d6768e48c7937518dfda5e2
Holyxey <text.holyxey@gmail.com>
2024-03-02 13:10:53 +0300

    js

--- JS.js
+++ JS.js
@@ -3,7 +3,28 @@ function holyxey() {
     document.documentElement.prepend(addCommentHolyxey);
 }
 //
-function hxFunSt() {
-    holyxey()
+function backUTMButton() {
+    if (location.href.includes('?utm_source=AdsTarget') && document.referrer.includes('adstarget')) {
+        let bb = document.createElement("a")
+        let referrerLink = document.referrer
+        bb.href = referrerLink
+        bb.style.cssText = `
+        position: fixed;
+        top: 2rem;
+        left: 2rem;
+        content: url("https://static.tildacdn.com/tild6663-6639-4166-a462-316239353461/SVG_logo_1.svg");
+        width: 2.5rem;
+        z-index: 995595607309995595607309;
+        background: #ffffff;
+        border-radius: 1rem;
+        padding: .5rem;
+        `;
+
+        document.documentElement.append(bb)
+    }
 }
-document.addEventListener("DOMContentLoaded", hxFunSt)
+//
+
+
+document.addEventListener("DOMContentLoaded", backUTMButton)
+document.addEventListener("DOMContentLoaded", holyxey)
