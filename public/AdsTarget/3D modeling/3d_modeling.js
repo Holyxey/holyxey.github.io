533e5c852a4017196b3104bef65aac01deb3f163
Holyxey <text.holyxey@gmail.com>
2024-03-10 20:26:14 +0300

    3d created

---
+++ public/AdsTarget/3D modeling/3d_modeling.js
@@ -0,0 +1,29 @@
+// CASES PARAMETERS ==========
+    let phCaseItem = document.getElementsByClassName('hx-at-photo-case-item')
+    for (let i = 0; i < phCaseItem.length; i++) {
+        phCaseItem[i].classList.add('case_' + (i + 1))
+        phCaseItem[i].style.cssText += ('background-image: url("https://holyxey.github.io/public/AdsTarget/3D modeling/imgs/' + (i + 1) + '.webp")')
+        phCaseItem[i].setAttribute('onclick', 'phCaseClick(this)')
+    }
+
+// CASES POPUP ==========
+    function phCaseClick(el) {
+        let image = el.style.backgroundImage;
+        let imageUrl = image.slice(5, -2);
+        let createPopupImg = document.createElement('img')
+        let createPopupDiv = document.createElement('div')
+
+        createPopupDiv.classList.add('hx-at-photo-popup-div')
+        createPopupDiv.setAttribute('onclick', 'phClosePopup(this)')
+
+        createPopupImg.classList.add('hx-at-photo-popup')
+        createPopupImg.src = imageUrl;
+        document.body.append(createPopupDiv)
+        createPopupDiv.append(createPopupImg)
+
+        document.documentElement.style.overflow = 'hidden'
+    }
+    function phClosePopup(el) {
+        document.addEventListener("click", el.remove())
+        document.documentElement.style.overflow = ''
+    }
