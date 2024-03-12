7f9dc716c04c6c1bfcb8324e428e6676cc461f2f
Holyxey <text.holyxey@gmail.com>
2024-03-12 06:02:36 +0300

    Create JSH.js

---
+++ JSH.js
@@ -0,0 +1,7 @@
+function wHolyxey() {
+    let addCommentHolyxey = document.createComment('Holyxey is working on the site');
+    document.documentElement.prepend(addCommentHolyxey);
+}
+//
+
+document.addEventListener("DOMContentLoaded", wHolyxey)
