3c390694ebdc64b120cc00c46bfdbffbb2714c04
Holyxey <text.holyxey@gmail.com>
2024-03-18 03:25:25 +0300

    at - firstCat

--- public/AdsTarget/atFirstCat.js
+++ public/AdsTarget/atFirstCat.js
@@ -1,5 +1,31 @@
-let atCat;
-let atCatIs = localStorage.getItem('_holyxeyCat');
+let atCat = localStorage.getItem('_atFirstCat');
+function setCat(cat) {
+    return function() {
+        if (cat && !atCat) {
+            localStorage.setItem('_atFirstCat', cat);
+            atCat = localStorage.getItem('_atFirstCat');
+            atCatPrior()
+        }
+    }
+}
+function atCatPrior() {
+    if (atCat) {
+        document.querySelectorAll('.hx-at-case-web[category="' + atCat + '"]').forEach(function (el) {
+            el.style.cssText += 'order: -10'
+            console.log(el)
+        })
+    }
+}
 
-let atCatServiceList = document.querySelectorAll('.hx-at-services-item');
-let atCatFilterList = document.querySelectorAll('[type="caseButton"]');
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.querySelectorAll('[type="caseButton"]').forEach(function (el) {
+        let cat = el.getAttribute('category')
+        el.addEventListener('click', setCat(cat))
+    })
+    document.querySelectorAll('.hx-at-services-item').forEach(function (el) {
+        let cat = el.getAttribute('category')
+        el.addEventListener('click', setCat(cat))
+    })
+    atCatPrior()
+})
