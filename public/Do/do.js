364115d88144f422a632a81b9008df8c6e6d2ca1
Holyxey <text.holyxey@gmail.com>
2024-03-21 04:41:28 +0300

    dododododo

--- public/Do/do.js
+++ public/Do/do.js
@@ -4,8 +4,11 @@ function logoLoaded() {
 
     let letter = document.querySelectorAll('.logoLetter')
     for (let i = 0; i < letter.length; i++) {
-        letter[i].style.animation = 'logoLetter .' + i + 's forwards ease-in-out'
+        letter[i].style.animation = 'logoLetter .' + i + 's forwards ease-in-out';
     }
+
+    document.querySelector('#DoStart').style.animation = 'DoStart 1.5s 2s ease-out forwards'
+    document.querySelector('.logoAfterLoad').style.animation = 'logoAfterLoad 3s 1s forwards ease'
 }
 window.addEventListener("load", function () {
     setTimeout(logoLoaded, 3000)
