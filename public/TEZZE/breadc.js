ad8ad77c8099c878f80039ef0e01a8195fad322d
Holyxey <text.holyxey@gmail.com>
2024-03-22 06:44:42 +0300

    Update breadc.js

--- public/TEZZE/breadc.js
+++ public/TEZZE/breadc.js
@@ -1,15 +1,36 @@
+let blockBread;
+let blockItemBread;
+let headerHeight;
+let tHeaderHeight;
+let headerBottom;
+let breadTop;
+let breadItemTop;
+let paddingDifference;
+let paddingSecondDifference;
+
+
 window.addEventListener("load", function () {
     setTimeout(function () {
-        let blockBread = document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode
-        let headerHeight = document.querySelector('[data-artboard-recid="633612477"]').clientHeight
-        let tHeaderHeight = document.querySelector('#t-header').clientHeight
+        blockBread = document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode
+        blockItemBread = document.querySelector('.t-rec_pb_15')
 
-        let headerBottom = document.querySelector('[data-artboard-recid="633612477"]').getBoundingClientRect().bottom
-        let breadTop = blockBread.getBoundingClientRect().top
+        headerHeight = document.querySelector('[data-artboard-recid="633612477"]').clientHeight
+        tHeaderHeight = document.querySelector('#t-header').clientHeight
 
-        let paddingDifference = (headerBottom - breadTop)
+        headerBottom = document.querySelector('[data-artboard-recid="633612477"]').getBoundingClientRect().bottom
+        breadTop = blockBread.getBoundingClientRect().top
+        breadItemTop = blockItemBread.getBoundingClientRect().top
 
-        blockBread.style.transition = 'all 1s ease-out'
-        blockBread.style.paddingTop = (paddingDifference + 'px')
+        paddingDifference = (headerBottom - breadTop)
+        paddingSecondDifference = (headerBottom - breadItemTop)
+
+        if (blockBread) {
+            blockBread.style.transition = 'all 1s ease-out'
+            blockBread.style.paddingTop = (paddingDifference + 'px')
+        }
+        if (blockItemBread) {
+            blockItemBread.style.transition = 'all 1s ease-out'
+            blockItemBread.style.paddingTop = (paddingSecondDifference + 'px')
+        }
     }, 3000)
 })
