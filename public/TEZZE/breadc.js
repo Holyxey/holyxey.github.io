b8f35559380fc713fef0e61590d15c8792713af8
Holyxey <text.holyxey@gmail.com>
2024-03-22 06:19:53 +0300

    Update breadc.js

--- public/TEZZE/breadc.js
+++ public/TEZZE/breadc.js
@@ -1,13 +1,13 @@
-let blockBread = document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode
-let headerHeight = document.querySelector('[data-artboard-recid="633612477"]').clientHeight
-let tHeaderHeight = document.querySelector('#t-header').clientHeight
-
-let headerBottom = document.querySelector('[data-artboard-recid="633612477"]').getBoundingClientRect().bottom
-let breadTop = blockBread.getBoundingClientRect().top
-
-let paddingDifference = (headerBottom - breadTop)
-
 window.addEventListener("load", function () {
+    let blockBread = document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode
+    let headerHeight = document.querySelector('[data-artboard-recid="633612477"]').clientHeight
+    let tHeaderHeight = document.querySelector('#t-header').clientHeight
+
+    let headerBottom = document.querySelector('[data-artboard-recid="633612477"]').getBoundingClientRect().bottom
+    let breadTop = blockBread.getBoundingClientRect().top
+
+    let paddingDifference = (headerBottom - breadTop)
+
     blockBread.style.transition = 'all .3s ease-out'
     blockBread.style.paddingTop = (paddingDifference + 'px')
 })
