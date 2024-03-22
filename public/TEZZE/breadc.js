33b97d9e9780b86001b30aadf5f94a3cf1a2d941
Holyxey <text.holyxey@gmail.com>
2024-03-22 09:01:52 +0300

    Update breadc.js

--- public/TEZZE/breadc.js
+++ public/TEZZE/breadc.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-    setTimeout(function () {
+    setInterval(function () {
         if (document.querySelector('[data-elem-id="1690796453018"]')) {
             let item = document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode;
             item.style.transition = 'all 1s ease-out'
@@ -9,5 +9,5 @@ window.addEventListener("load", function () {
             document.querySelector('.t-rec_pb_15').style.transition = 'all 1s ease-out'
             document.querySelector('.t-rec_pb_15').style.paddingTop = ((document.querySelector('[data-artboard-recid="633612477"]').getBoundingClientRect().height - document.querySelector('.t-rec_pb_15').offsetTop) + 15 + 'px')
         }
-    }, 3000)
+    }, 1000)
 })
