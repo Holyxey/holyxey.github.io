06e82cac8c3474870bebaf425fc3bcb99e87a1ec
Holyxey <text.holyxey@gmail.com>
2024-03-22 06:57:00 +0300

    Update breadc.js

--- public/TEZZE/breadc.js
+++ public/TEZZE/breadc.js
@@ -1,12 +1,13 @@
 window.addEventListener("load", function () {
     setTimeout(function () {
-        if (document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode) {
-            document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode.style.transition = 'all 1s ease-out'
-            document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode.style.paddingTop = ((document.querySelector('[data-artboard-recid="633612477"]').getBoundingClientRect().bottom - document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode.getBoundingClientRect().top) + 'px')
+        if (document.querySelector('[data-elem-id="1690796453018"]')) {
+            let item = document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode;
+            item.style.transition = 'all 1s ease-out'
+            item.style.paddingTop = ((document.querySelector('[data-artboard-recid="633612477"]').getBoundingClientRect().bottom - item.getBoundingClientRect().top) + 'px')
         }
         if (document.querySelector('.t-rec_pb_15')) {
             document.querySelector('.t-rec_pb_15').style.transition = 'all 1s ease-out'
-            document.querySelector('.t-rec_pb_15').style.paddingTop = ((document.querySelector('[data-artboard-recid="633612477"]').getBoundingClientRect().bottom - document.querySelector('.t-rec_pb_15').getBoundingClientRect().top) + 'px')
+            document.querySelector('.t-rec_pb_15').style.paddingTop = ((document.querySelector('[data-artboard-recid="633612477"]').getBoundingClientRect().bottom - document.querySelector('.t-rec_pb_15').getBoundingClientRect().top) + 15 + 'px')
         }
     }, 3000)
 })
