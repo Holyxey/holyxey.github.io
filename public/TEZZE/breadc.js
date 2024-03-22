b592d2c062163be3cac0a15cbe91a5849ca01f74
Holyxey <text.holyxey@gmail.com>
2024-03-22 06:52:40 +0300

    Update breadc.js

--- public/TEZZE/breadc.js
+++ public/TEZZE/breadc.js
@@ -1,36 +1,12 @@
-let blockBread;
-let blockItemBread;
-let headerHeight;
-let tHeaderHeight;
-let headerBottom;
-let breadTop;
-let breadItemTop;
-let paddingDifference;
-let paddingSecondDifference;
-
-
 window.addEventListener("load", function () {
     setTimeout(function () {
-        blockBread = document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode
-        blockItemBread = document.querySelector('.t-rec_pb_15')
-
-        headerHeight = document.querySelector('[data-artboard-recid="633612477"]').clientHeight
-        tHeaderHeight = document.querySelector('#t-header').clientHeight
-
-        headerBottom = document.querySelector('[data-artboard-recid="633612477"]').getBoundingClientRect().bottom
-        breadTop = blockBread.getBoundingClientRect().top
-        breadItemTop = blockItemBread.getBoundingClientRect().top
-
-        paddingDifference = (headerBottom - breadTop)
-        paddingSecondDifference = (headerBottom - breadItemTop)
-
-        if (blockBread) {
-            blockBread.style.transition = 'all 1s ease-out'
-            blockBread.style.paddingTop = (paddingDifference + 'px')
+        if (document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode) {
+            document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode.style.transition = 'all 1s ease-out'
+            document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode.style.paddingTop = ((document.querySelector('[data-artboard-recid="633612477"]').getBoundingClientRect().bottom - document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode.getBoundingClientRect().top) + 'px')
         }
-        if (blockItemBread) {
-            blockItemBread.style.transition = 'all 1s ease-out'
-            blockItemBread.style.paddingTop = (paddingSecondDifference + 'px')
+        if (document.querySelector('.t-rec_pb_15')) {
+            document.querySelector('.t-rec_pb_15').style.transition = 'all 1s ease-out'
+            document.querySelector('.t-rec_pb_15').style.paddingTop = ((document.querySelector('[data-artboard-recid="633612477"]').getBoundingClientRect().bottom - document.querySelector('.t-rec_pb_15').getBoundingClientRect().top) + 'px')
         }
     }, 3000)
 })
