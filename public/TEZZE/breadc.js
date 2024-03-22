2091a49b8518c5c32ae5c1d26363d1a3ac7e0ac2
Holyxey <text.holyxey@gmail.com>
2024-03-22 06:03:22 +0300

    Update breadc.js

--- public/TEZZE/breadc.js
+++ public/TEZZE/breadc.js
@@ -3,13 +3,14 @@ let headerHeight = document.querySelector('[data-artboard-recid="633612477"]').c
 let tHeaderHeight = document.querySelector('#t-header').clientHeight
 
 window.addEventListener("load", function () {
-    // blockBread.style.paddingTop = ((headerHeight - tHeaderHeight) + 'px')
-    if (headerHeight > tHeaderHeight) {
-        blockBread.style.paddingTop = ((headerHeight - tHeaderHeight) + 'px')
-        blockBread.style.paddingTop = ((headerHeight) + 'px')
-        blockBread.style.transition = 'all .3s ease-out'
-    } else {
-        blockBread.style.paddingTop = ((headerHeight) + 'px')
-        blockBread.style.transition = 'all .3s ease-out'
-    }
+    setTimeout(function () {
+        if (headerHeight > tHeaderHeight) {
+            blockBread.style.paddingTop = ((headerHeight - tHeaderHeight) + 'px')
+            blockBread.style.paddingTop = ((headerHeight) + 'px')
+            blockBread.style.transition = 'all .3s ease-out'
+        } else {
+            blockBread.style.paddingTop = ((headerHeight) + 'px')
+            blockBread.style.transition = 'all .3s ease-out'
+        }
+    }, 1500)
 })
