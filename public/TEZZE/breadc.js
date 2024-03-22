e7ad80a3a218cfa16b4c193b0fc1908f72ebb661
Holyxey <text.holyxey@gmail.com>
2024-03-22 06:16:32 +0300

    Update breadc.js

--- public/TEZZE/breadc.js
+++ public/TEZZE/breadc.js
@@ -1,4 +1,4 @@
-let blockBread = document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode.parentNode
+let blockBread = document.querySelector('[data-elem-id="1690796453018"]').parentNode.parentNode
 let headerHeight = document.querySelector('[data-artboard-recid="633612477"]').clientHeight
 let tHeaderHeight = document.querySelector('#t-header').clientHeight
 
