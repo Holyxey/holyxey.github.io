f3b6dd735acb72bbb378f32a802a450d56768b41
Holyxey <text.holyxey@gmail.com>
2024-04-02 15:42:40 +0300

    dodotutu

--- public/Do/do.js
+++ public/Do/do.js
@@ -5,12 +5,24 @@ window.addEventListener("load", function () {
             document.querySelector('#tildacopy').remove()
     }, 1000)
 
-    if (document.querySelector('.wTime') && document.querySelector('.address')) {
-        let d = document.querySelector('.wTime');
-        let a = document.querySelector('.address');
-        d.style.top = a.getBoundingClientRect().height + 'px'
-        window.addEventListener("resize", ()=> {
-            d.style.top = a.getBoundingClientRect().height + 'px'
-        })
-    }
+    // if (document.querySelector('.wTime') !== null && document.querySelector('.address') !== null) {
+    //     if (document.querySelector('.phoneNumber') !== null) {
+    //         let d = document.querySelector('.wTime');
+    //         let a = document.querySelector('.address');
+    //         let p = document.querySelector('.phoneNumber');
+    //         p.style.top = a.getBoundingClientRect().height + 'px'
+    //         d.style.top = a.getBoundingClientRect().height + p.getBoundingClientRect().height + 'px'
+    //         window.addEventListener("resize", ()=> {
+    //             p.style.top = a.getBoundingClientRect().height + 'px'
+    //             d.style.top = a.getBoundingClientRect().height + p.getBoundingClientRect().height + 'px'
+    //         })
+    //     } else {
+    //         let d = document.querySelector('.wTime');
+    //         let a = document.querySelector('.address');
+    //         d.style.top = a.getBoundingClientRect().height + 'px'
+    //         window.addEventListener("resize", ()=> {
+    //             d.style.top = a.getBoundingClientRect().height + 'px'
+    //         })
+    //     }
+    // }
 })
