6fd6af8527eefe9247b4cc851cea89a93a0c359a
Holyxey <text.holyxey@gmail.com>
2024-04-13 13:15:57 +0300

    scr

--- scratch/js.js
+++ scratch/js.js
@@ -4,21 +4,13 @@ let articles = document.querySelectorAll('article')
 
 const randomNumber = (max) => {
     if (max)
-        return (Math.random() * max - 1)
-}
-const randomDeg = (el) => {
-    if (el) {
-        el.style.transform = `rotate(${randomNumber(2)}deg)`;
-    }
+        return Math.floor(Math.random() * max)
 }
 const showArticle = (el) => {
-    if (el.getBoundingClientRect().top < (window.innerHeight - 50) && el.getBoundingClientRect().bottom > -50) {
-        // setInterval(() => {
-            el.style.cssText = 'animation: showArticle .8s ease-out forwards';
-        // }, randomNumber(100))
-
+    if (el.getBoundingClientRect().top < (window.innerHeight - 50) && el.getBoundingClientRect().bottom > 50) {
+        el.style.cssText += 'animation: showArticleLeft .8s ease-out forwards';
     } else
-        el.style.cssText = 'animation: hideArticle .5s ease-out forwards'
+        el.style.cssText += 'animation: hideArticle .5s ease-out forwards'
 }
 const showUpArrow = () => {
     let arrow = document.querySelector('#up')
@@ -33,7 +25,6 @@ const hideUpArrow = () => {
 
 articles.forEach((el) => {
     showArticle(el)
-    randomDeg(el)
 })
 window.addEventListener("scroll", () => {
     articles.forEach((el) => {
