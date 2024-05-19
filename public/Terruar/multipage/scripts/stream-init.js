6fa20a8c8ece554ba4195525c9477aa17465918c
Holyxey <text.holyxey@gmail.com>
2024-05-19 20:02:11 +0300

    terr

--- public/Terruar/multipage/scripts/stream-init.js
+++ public/Terruar/multipage/scripts/stream-init.js
@@ -3,21 +3,25 @@ let sldpPlayer;
 let getWidth;
 let getHeigth;
 
-function initPlayer() {
-    getWidth = document.getElementById('video-stream').width
-    getHeigth = (getWidth / 16) * 9
+document.addEventListener("DOMContentLoaded", initPlayer);
 
+function initPlayer() {
+    getWidth = document.getElementById('video-stream').offsetWidth
+    getHeigth = (getWidth / 16) * 9
     sldpPlayer = SLDP.init({
         container: 'sldp_player_wrapper',
-        stream_url: "wss://b2o-vcore155.video.goodline.info:443/main/linkvideo_125009",
-        width: `${getWidth}`,
-        height: `${getHeigth}`,
-        muted: true,
-        autoplay: true
+        sync_buffer: 1000,
+        log_level:   'debug',
+        stream_url: 'wss://b2o-vcore155.video.goodline.info:443/main/linkvideo_125009',
+        buffering: 1200,
+        autoplay: true,
+        height: getHeigth,
+        width: getWidth
     });
 }
 
 function removePlayer() {
-    sldpPlayer.destroy();
+    sldpPlayer.destroy(function () {
+        console.log('SLDP Player is destroyed.');
+    });
 }
-
