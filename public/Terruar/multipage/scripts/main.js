dcceadb3ea00ce775aa3a83b2c31de9b9c4bf4a7
Holyxey <text.holyxey@gmail.com>
2024-10-11 04:23:14 +0400

    Update main.js

--- public/Terruar/multipage/scripts/main.js
+++ public/Terruar/multipage/scripts/main.js
@@ -959,15 +959,6 @@ const styleClassLists = [
 ]
 
 //
-const whatIsMax = function(img) {
-    if (img.width / img.height <= img.parentNode.clientWidth / img.parentNode.clientHeight) {
-        img.style.maxHeight = 'unset'
-        img.style.maxWidth = '100%'
-    } else {
-        img.style.maxHeight = '100%'
-        img.style.maxWidth = 'unset'
-    }
-}
 const needToRender = function (where) {
     const sections = document.querySelectorAll('[data-need-to-render]')
 
