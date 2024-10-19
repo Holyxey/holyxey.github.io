bd993e1fdeb7cd9bb53099bf00a12cccf622a7ad
Holyxey <text.holyxey@gmail.com>
2024-10-19 07:22:26 +0400

    welton - wedding

---
+++ public/Welton/Wedding/wedding.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const highlights = [
+        {
+            eng: 'Complement from Executive Chief – fruit plate and dessert, accompanied by a bottle of sparkling wine',
+            res: 'Комплемент от бренд-шефа ресторана: фруктово-десертный набор и игристое вино'
+        },
+        {
+            eng: 'In-room breakfast (special menu)',
+            res: 'Поднятие завтрака в номер ( по специальному меню )'
+        },
+        {
+            eng: 'Decoration of the room with rose petals',
+            res: 'Украшение номера лепестками'
+        },
+        {
+            eng: 'Mr. & Mrs. bathrobes',
+            res: 'Халаты Mr & Mrs'
+        },
+    ]
+    const hlNode = document.querySelector('#weddingHighLight > ul'); // todo замени на en.
+    (()=>{
+        const host = location.hostname.startsWith('local') ? 'eng' : 'rus'
+        highlights.forEach(highlight=>{
+            hlNode.insertAdjacentHTML('beforeend', `<li>${highlight[host]}</li>`)
+        })
+    })()
+})
