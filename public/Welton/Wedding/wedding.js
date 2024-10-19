395bba2f70f91b064b01658a74e7b9328c9e434e
Holyxey <text.holyxey@gmail.com>
2024-10-19 07:31:13 +0400

    Update wedding.js

--- public/Welton/Wedding/wedding.js
+++ public/Welton/Wedding/wedding.js
@@ -1,26 +1,26 @@
 const highlights = [
     {
         eng: 'Complement from Executive Chief – fruit plate and dessert, accompanied by a bottle of sparkling wine',
-        res: 'Комплемент от бренд-шефа ресторана: фруктово-десертный набор и игристое вино'
+        rus: 'Комплемент от бренд-шефа ресторана: фруктово-десертный набор и игристое вино'
     },
     {
         eng: 'In-room breakfast (special menu)',
-        res: 'Поднятие завтрака в номер ( по специальному меню )'
+        rus: 'Поднятие завтрака в номер ( по специальному меню )'
     },
     {
         eng: 'Decoration of the room with rose petals',
-        res: 'Украшение номера лепестками'
+        rus: 'Украшение номера лепестками'
     },
     {
         eng: 'Mr. & Mrs. bathrobes',
-        res: 'Халаты Mr & Mrs'
+        rus: 'Халаты Mr & Mrs'
     },
 ]
 
 document.addEventListener('DOMContentLoaded', () => {
-    const hlNode = document.querySelector('#weddingHighLight > ul'); // todo замени на en.
+    const hlNode = document.querySelector('#weddingHighLight > ul');
     (()=>{
-        const host = location.hostname.startsWith('local') ? 'eng' : 'rus'
+        const host = location.hostname.startsWith('en.') ? 'eng' : 'rus' // todo замени на en.
         highlights.forEach(highlight=>{
             hlNode.insertAdjacentHTML('beforeend', `<li>${highlight[host]}</li>`)
         })
