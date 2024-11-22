f309b1e2ba919666dd459f145056120c91cabb70
Holyxey <text.holyxey@gmail.com>
2024-11-22 16:05:00 +0400

    AdsTarget - partners

---
+++ public/AdsTarget/partneres/partnersList.js
@@ -0,0 +1,212 @@
+const partners = [
+    {
+        name: 'STUDIOCONTEXT',
+        link: 'https://studiocontext.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/STUDIOCONTEXT.webp',
+    },
+    {
+        name: 'DUPLEX',
+        link: 'https://1715hotel.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/DUPLEX.webp',
+    },
+    {
+        name: 'KIDKORT',
+        link: 'https://kidkort.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/KIDKORT.webp',
+    },
+    {
+        name: 'LESIMORE',
+        link: 'https://lesimore.com/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/LESIMORE.webp',
+    },
+    {
+        name: 'NEWSCENE',
+        link: 'https://scene-spb.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/NEWSCENE.webp',
+    },
+    {
+        name: 'THEGARDEN',
+        link: 'https://thegarden.camp/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/THEGARDEN.webp',
+    },
+    {
+        name: 'AUNA',
+        link: 'https://www.ayna-spb.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/AUNA.webp',
+    },
+    {
+        name: 'CHOKOROOM',
+        link: 'https://chocoroom62.com/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/CHOKOROOM.webp',
+    },
+    {
+        name: 'PEREKRESTOK',
+        link: 'https://www.perekrestok.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/PEREKRESTOK.webp',
+    },
+    {
+        name: '5KA',
+        link: 'https://5ka.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/5KA.webp',
+    },
+    {
+        name: 'MITSUBISHI',
+        link: 'https://www.mitsubishi-motors.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/MITSUBISHI.webp',
+    },
+    {
+        name: 'SKILLBOX',
+        link: 'https://skillbox.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/SKILLBOX.webp',
+    },
+    {
+        name: 'hh',
+        link: 'https://hh.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/hh.webp',
+    },
+    {
+        name: 'STAKE',
+        link: 'https://steakdavay.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/STAKE.webp',
+    },
+    {
+        name: 'EURASIA',
+        link: 'https://evrasia.spb.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/EURASIA.webp',
+    },
+    {
+        name: 'SBER',
+        link: 'https://www.sberbank.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/SBER.webp',
+    },
+    {
+        name: 'LATUAL',
+        link: 'https://www.letu.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/LATUAL.webp',
+    },
+    {
+        name: 'TIHIY',
+        link: 'https://tihiysushi.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/TIHIY.webp',
+    },
+    {
+        name: 'ITHUB',
+        link: 'https://ithub.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/ITHUB.webp',
+    },
+    {
+        name: 'KANNONDANCE',
+        link: 'https://kannondance.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/KANNONDANCE.webp',
+    },
+    {
+        name: 'MOUSEHOUSE',
+        link: 'https://m-house.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/MOUSEHOUSE.webp',
+    },
+    {
+        name: 'LATRIKA',
+        link: 'https://latrika.com/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/LATRIKA.webp',
+    },
+    {
+        name: 'OPENLOOK',
+        link: 'https://open-look.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/OPENLOOK.webp',
+    },
+    {
+        name: 'TEMPI',
+        link: 'https://tempi.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/TEMPI.webp',
+    },
+    {
+        name: 'WELTON',
+        link: 'https://weltonhotel.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/WELTON.webp',
+    },
+    {
+        name: 'HTI',
+        link: 'https://hti-group.kg/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/HTI.webp',
+    },
+    {
+        name: 'STRAWBERRY',
+        link: 'https://strawberryfields-spb.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/STRAWBERRY.webp',
+    },
+    {
+        name: 'VOKZAL',
+        link: 'https://vokzal1853.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/VOKZAL.webp',
+    },
+    {
+        name: 'JEWELLERKARAT',
+        link: 'https://jeweller-karat.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/JEWELLERKARAT.webp',
+    },
+    {
+        name: 'KUPELI',
+        link: 'https://kupel-v-metel.ru/?utm_source=AdsTarget',
+        image: 'https://holyxey.github.io/public/AdsTarget/videoproduction/partners/KUPELI.webp',
+    },
+]
+const partnersNode = document.querySelector('#partnersNode');
+
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+(function renderPartners() {
+    partnersNode.insertAdjacentHTML('beforeend', `<div class="swiper-wrapper"></div>`)
+    partnersNode.insertAdjacentHTML('beforeend', `<div class="swiper-button-prev"></div>`)
+    partnersNode.insertAdjacentHTML('beforeend', `<div class="swiper-button-next"></div>`)
+
+
+    const wrapper = partnersNode.querySelector('.swiper-wrapper')
+    partners.map(el => {
+        wrapper.insertAdjacentHTML('beforeend',
+            `<div class="swiper-slide">
+                    <a href="${el.link}" aria-label="${el.name}" target="_blank">
+                        <img src="${el.image}" alt="Партнер AdsTarget ${el.name}" />
+                    </a>
+                </div>`)
+    })
+})()
+
+
+const slidesPerView = partnersNode.getBoundingClientRect().width / 200
+
+const swiperParams = {
+    speed: 400,
+    endless: true,
+    loop: true,
+    autoplay: { delay: 2400 },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        100: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+            grid: {
+                rows: 2
+            }
+        },
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 8,
+            spaceBetween: 50,
+        },
+    },
+}
+const swiper = new Swiper('.swiper', swiperParams);
