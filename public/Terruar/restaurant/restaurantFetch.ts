2c20d41851eb3f85aea04d88c05d278f1fecde18
Holyxey <text.holyxey@gmail.com>
2025-07-20 06:15:20 +0400

    Terruar: fix: cat link

--- public/Terruar/restaurant/restaurantFetch.ts
+++ public/Terruar/restaurant/restaurantFetch.ts
@@ -20,7 +20,7 @@ function createOptimLink(link: string): string {
 
 export async function getProductsByType(): Promise<Product[]> {
   const link: string =
-    'https://store.tildaaapi.com/api/getproductslist/?storepartuid=513452425241&recid=752306605&c=1716436482010&getparts=true&getoptions=true&slice=1&size=36&projectid=5131025';
+    'https://store.tildaapi.com/api/getproductslist/?storepartuid=513452425241&recid=752306605&c=1716436482010&getparts=true&getoptions=true&slice=1&size=36&projectid=5131025';
 
   let restaurantData;
 
